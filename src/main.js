8b29f72e2e6d1d328f6e95ed6377a39abf1c12c4
TNoga0 <t.noga0@gmail.com>
2022-01-19 20:36:54 +0100

    Add axios for API requests

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import Axios from 'axios';
+
 Vue.config.productionTip = false;
+
+Vue.prototype.$http = Axios;
 
 new Vue({
   router,
