017eb654d94b4b1748b32f17d4361706d669d560
TNoga0 <t.noga0@gmail.com>
2022-01-20 21:37:43 +0100

    Properly style and color people font

--- src/components/data_presentation/SinglePerson.vue
+++ src/components/data_presentation/SinglePerson.vue
@@ -1,35 +1,37 @@
 <template>
   <div class="single-person" :style="cssVars">
-    {{personData.name}} {{personData.eye_color}}
+    <p class="dark-text-outline"
+       v-if="eyeColor === 'black'">
+      {{personData.name}}
+    </p>
+    <p v-else>{{personData.name}}</p>
   </div>
 </template>
 
 <script>
+  import { eyeColors } from '@/components/data_handlers/replacement_data.json';
+
   export default {
     name: 'SinglePerson',
     props: { personData: Object },
     data() {
-      return {};
+      return { punctuationSymbols: [',', '-'] };
     },
     computed: {
       eyeColor() {
         const { eye_color, ...rest } = this.personData;
-        if (eye_color.split(',').length === 1 && eye_color.split('-').length === 1) {
-          switch (eye_color) {
-          case 'hazel':
-            return 'brown';
-          case 'unknown':
-            return 'white';
-          default:
-            return eye_color;
+        let symbolSplits = [];
+        let colorToReturn = '';
+        this.punctuationSymbols.forEach((symbol, index) => {
+          symbolSplits.push(eye_color.split(symbol));
+          if (symbolSplits[index].length > 1) {
+            colorToReturn = eye_color.split(symbol)[0];
           }
+        });
+        if (symbolSplits[0].length === 1 && symbolSplits[1].length === 1) {
+          return Object.keys(eyeColors).includes(eye_color) ? eyeColors[eye_color] : eye_color;
         }
-        else if (eye_color.split(',').length > 1) {
-          return eye_color.split(',')[0];
-        }
-        else if (eye_color.split('-').length > 1) {
-          return eye_color.split('-')[0];
-        }
+        return colorToReturn;
       },
       cssVars() {
         return { '--color': this.eyeColor };
@@ -45,16 +47,20 @@
   justify-content: space-around;
   margin: auto;
   padding-top: 0.9vh;
-  font-size: clamp(0.7rem, 5vw, 2rem);
-  /*color: var(--color);*/
+  font-size: clamp(0.7rem, 7vw, 2rem);
+  color: var(--color);
   /*text-shadow: 0 0 5px var(--color);*/
-  text-shadow:
-    0 0 7px #fff,
-    0 0 10px var(--color),
-    0 0 20px var(--color);
-  color: white;
+  /*text-shadow:*/
+  /*  0 0 7px #fff,*/
+  /*  0 0 10px var(--color),*/
+  /*  0 0 20px var(--color);*/
+  /*color: white;*/
   width: 70vw;
   height: 4vh;
+}
+
+.dark-text-outline {
+  text-shadow: 0 0 1px yellow, 0 0 1px yellow, 0 0 1px yellow;
 }
 
 </style>
